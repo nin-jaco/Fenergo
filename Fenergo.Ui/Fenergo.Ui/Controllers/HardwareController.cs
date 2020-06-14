e445988ecdf4dd07b1ed831360bb900b980df961
jaco <jaco.kleynhans@icloud.com>
2020-06-14 23:33:00 +0200

    Hooking up form

--- Fenergo.Ui/Fenergo.Ui/Controllers/HardwareController.cs
+++ Fenergo.Ui/Fenergo.Ui/Controllers/HardwareController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Fenergo.Ui.Dtos;
+using Fenergo.Ui.ViewModels;
 
 namespace Fenergo.Ui.Controllers
 {
@@ -13,5 +15,54 @@ namespace Fenergo.Ui.Controllers
         {
             return View();
         }
+
+        public ActionResult Edit(int id)
+        {
+            //var customer = Context.Customers.FirstOrDefault(p => p.Id == id);
+            //if (customer == null) return HttpNotFound();
+
+            var viewModel = new HardwareFormViewModel
+            {
+                //Customer = customer,
+                //MembershipTypes = Context.MembershipTypes.ToList()
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(HardwareDto hardwareDto)
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    var viewModel = new HardwareFormViewModel
+            //    {
+            //        Customer = customer,
+            //        MembershipTypes = Context.MembershipTypes.ToList()
+            //    };
+            //    return View("CustomerForm", viewModel);
+            //}
+
+            //if (customer.Id == 0)
+            //{
+            //    Context.Customers.Add(customer);
+            //}
+            //else
+            //{
+            //    var dbCust = Context.Customers.FirstOrDefault(p => p.Id == customer.Id);
+            //    //TryUpdateModel(dbCust);
+            //    //automapper
+            //    dbCust.Name = customer.Name;
+            //    dbCust.DateOfBirth = customer.DateOfBirth;
+            //    dbCust.MembershipTypeId = customer.MembershipTypeId;
+            //    dbCust.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            //}
+
+            //Context.SaveChanges();
+
+            return RedirectToAction("Index", "Hardware");
+        }
+
+        
     }
 }
---
+++ Fenergo.Ui/Fenergo.Ui/ViewModels/HardwareFormViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Fenergo.Ui.Dtos;
+using Fenergo.Ui.Models;
+
+namespace Fenergo.Ui.ViewModels
+{
+    public class HardwareFormViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public byte IdHardwareType { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public string SerialNumber { get; set; }
+        public Photo Photo { get; set; }
+        public int IdPhoto { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public IEnumerable<HardwareTypeDto> HardwareTypes { get; set; }
+    }
+}
