621d9d6fd454a20bd98e8a84358c8fdbc48bbec1
jaco <jaco.kleynhans@icloud.com>
2020-07-14 20:51:44 +0200

    Bug fixes

--- Fenergo.Ui/Fenergo.Ui/App_Start/BundleConfig.cs
+++ Fenergo.Ui/Fenergo.Ui/App_Start/BundleConfig.cs
@@ -9,16 +9,17 @@ namespace Fenergo.Ui
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/lib").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/bootstrap.js",
-                        "~/Scripts/bootbox.js",
-                        "~/Scripts/datatables/jquery.datatables.js",
-                        "~/Scripts/datatables/datatables.bootstrap.js",
-            "~/Scripts/typeahead.bundle.js",
-            "~/Scripts/toastr.js"));
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/bootbox.js",
+                "~/Scripts/datatables/jquery.datatables.js",
+                "~/Scripts/datatables/datatables.bootstrap.js",
+                "~/Scripts/typeahead.bundle.js",
+                "~/Scripts/toastr.js"
+            ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -26,11 +27,11 @@ namespace Fenergo.Ui
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/datatables/css/datatables.bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/typeAhead.css",
-                      "~/Content/toastr.css"));
+                "~/Content/bootstrap.css",
+                "~/Content/site.css",
+                "~/Content/datatables/css/datatables.bootstrap.css",
+                "~/Content/typeAhead.css",
+                "~/Content/toastr.css"));
         }
     }
 }
--- Fenergo.Ui/Fenergo.Ui/Controllers/HardwareController.cs
+++ Fenergo.Ui/Fenergo.Ui/Controllers/HardwareController.cs
@@ -16,53 +16,12 @@ namespace Fenergo.Ui.Controllers
             return View();
         }
 
-        public ActionResult Edit(int id)
+        public ActionResult Edit()
         {
-            //var customer = Context.Customers.FirstOrDefault(p => p.Id == id);
-            //if (customer == null) return HttpNotFound();
-
-            var viewModel = new HardwareFormViewModel
-            {
-                //Customer = customer,
-                //MembershipTypes = Context.MembershipTypes.ToList()
-            };
-            return View(viewModel);
+            return View();
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Save(HardwareDto hardwareDto)
-        {
-            //if (!ModelState.IsValid)
-            //{
-            //    var viewModel = new HardwareFormViewModel
-            //    {
-            //        Customer = customer,
-            //        MembershipTypes = Context.MembershipTypes.ToList()
-            //    };
-            //    return View("CustomerForm", viewModel);
-            //}
-
-            //if (customer.Id == 0)
-            //{
-            //    Context.Customers.Add(customer);
-            //}
-            //else
-            //{
-            //    var dbCust = Context.Customers.FirstOrDefault(p => p.Id == customer.Id);
-            //    //TryUpdateModel(dbCust);
-            //    //automapper
-            //    dbCust.Name = customer.Name;
-            //    dbCust.DateOfBirth = customer.DateOfBirth;
-            //    dbCust.MembershipTypeId = customer.MembershipTypeId;
-            //    dbCust.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
-            //}
-
-            //Context.SaveChanges();
-
-            return RedirectToAction("Index", "Hardware");
-        }
-
+        
         
     }
 }
